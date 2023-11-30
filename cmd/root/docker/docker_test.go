9e00957e1dfbe2367999bbbf7e714442c1bdd87e
Frederik Jobst <frederik.jobst@softwareag.com>
2023-11-30 15:00:59 +0100

    Fixed Docker Exec Test and removed double license header

--- cmd/root/docker/docker_test.go
+++ cmd/root/docker/docker_test.go
@@ -1,17 +1,3 @@
-//	Copyright 2023 Dremio Corporation
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//	http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 //  Copyright 2023 Dremio Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -61,7 +47,7 @@ func TestDockerExec(t *testing.T) {
 	if len(calls) != 1 {
 		t.Errorf("expected 1 call but got %v", len(calls))
 	}
-	expectedCall := []string{"docker", "exec","--user", "root", "localdremioincontainer", "ls", "-l"}
+	expectedCall := []string{"docker", "exec", "localdremioincontainer", "ls", "-l"}
 	if !reflect.DeepEqual(calls[0], expectedCall) {
 		t.Errorf("\nexpected call\n%v\nbut got\n%v", expectedCall, calls[0])
 	}
